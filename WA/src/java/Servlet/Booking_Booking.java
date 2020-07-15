4128c5c484afeae35f0f5679054ba67106949183
lytie <vojkeiomn@gmail.com>
2020-07-15 22:31:11 +0700

    up

---
+++ WA/src/java/Servlet/Booking_Booking.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlet;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author longly
+ */
+@Named(value = "booking_Booking")
+@Dependent
+public class Booking_Booking {
+    
+
+    /**
+     * Creates a new instance of Booking_Booking
+     */
+    public Booking_Booking() {
+    }
+    
+}
---
+++ WA/src/java/bean/common.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+/**
+ *
+ * @author longly
+ */
+public class common {
+     public static String url="http://localhost:8080/WA/Booking";
+     
+    public static String urlImg="http://localhost:8080/WA/images";
+}

b3ca25ce619cfa2053615b421fc4138449e5daf5
batzar <Bhcjkgfhjg@gmail.com>
2020-07-20 12:53:36 +0700

    update qrcodegen example

--- Paypal/src/java/newpackage/QrcodeGen.java
+++ Paypal/src/java/newpackage/QrcodeGen.java
@@ -6,10 +6,35 @@
 
 package newpackage;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 /**
  *
  * @author Admin
  */
 public class QrcodeGen {
-    
+        //String data = "https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=EC-9GX22361GM8363508";
+
+    public void createQr(String data,String imgname,String filetype) throws WriterException, IOException{
+        
+    QRCodeWriter qrCodeWriter = new QRCodeWriter();
+    BitMatrix matrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, 200, 200);
+
+    // Write to file image
+    String outputFile = "C:/Users/Admin/Desktop/"+imgname+"."+filetype;
+    Path path = FileSystems.getDefault().getPath(outputFile);
+    MatrixToImageWriter.writeToPath(matrix, "PNG", path);
+    // Write to byte array
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    MatrixToImageWriter.writeToStream(matrix, "PNG", outputStream);
+    byte[] pngByteArray = outputStream.toByteArray();
+    }
 }
